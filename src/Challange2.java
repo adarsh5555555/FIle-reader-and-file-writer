b5cb9ebc72a85fc0ad5bf68cfec2b0ab8810729d
Adarsh <adarsh80810@gmail.com>
2025-05-30 01:19:51 +0530

    Add these to my repositry

---
+++ src/Challange2.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Challange2 {
+    public static void main(String[] args) {
+        List<Integer> myList = new ArrayList<>();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(4);
+       
+    }
+
+}
--- src/Collectionchallanges.java
+++ src/Collectionchallanges.java
@@ -1,2 +1,14 @@
 public class Collectionchallanges {
+    public static void main(String[] args) {
+        String ss = var("this ","is ","my ","codebase");
+        System.out.println(ss);
+
+    }
+    public static String var(String...str){
+        String strrr="";
+        for(String strr:str){
+            strrr =strrr+strr;
+        }
+        return strrr;
+    }
 }
